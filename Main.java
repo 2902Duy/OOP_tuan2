3ec9a04d5c3239f1cbb2f4c200ff9daef35e5a1e
Trinh Duy <tduy29.2k4@gmail.com>
2024-02-29 09:35:04 +0700

    push

---
+++ Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        person duy = new person(1,"Duy",20,65);
+        person triet = new person(2,"Triet",13,50);
+        person khoa = new person(3,"Khoa",20,70);
+    }
+}
